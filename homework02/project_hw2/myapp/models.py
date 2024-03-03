18041234fd1e63b7b08a99e9793014aa3fa11ad9
mrivs <valentinilyins@gmail.com>
2024-03-04 02:02:59 +0300

    homework06 fin+

--- homework02/project_hw2/myapp/models.py
+++ homework02/project_hw2/myapp/models.py
@@ -50,7 +50,7 @@ class Product(models.Model):
 class Order(models.Model):
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
     products = models.ManyToManyField(Product)
-    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2,default=0)
     date_ordered = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
